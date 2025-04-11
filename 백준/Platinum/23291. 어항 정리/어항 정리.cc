647d36f9308fa61a60fed52825d3f06e08fa6e18
박준일_T7154 <116266030+june21a@users.noreply.github.com>
2025-04-12 03:06:47 +0900

    [Platinum V] Title: 어항 정리, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.cc
+++ 백준/Platinum/23291. 어항 정리/어항 정리.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define ll long long
-#define DIV 5
 #define MAX_VALUE 50001
 #define EMPTY -1
+#define vi vector<int>
+#define vvi vector<vector<int>>
 
 using namespace std;
 
 int N, K;
-int start_idx, end_idx;
-vector<vector<int>> v(200, vector<int>(200, EMPTY));
+int s=0;
+vvi v(200, vi(200, EMPTY));
 
 int get_diff(){
     int m = MAX_VALUE, M=0;
@@ -21,42 +21,38 @@ int get_diff(){
     return (M-m);
 }
 
-int get_base_length(){
-    return (end_idx - start_idx + 1);
-}
-
 void minus_all_min_idx(){
     int m = MAX_VALUE;
     for(int i=0; i<N; i++) m = min(m, v[i][0]);
     for(int i=0; i<N; i++) if(v[i][0] == m) v[i][0]++;
 }
 
-bool check_first_list_up_pos(){
-    int base_length = get_base_length();
+bool check_first_lift_up_pos(){
+    int l1 = N - s;
 
     int cnt = 0;
-    for(int i=start_idx; i<=end_idx; i++) if(v[i][1] != EMPTY) cnt++;
+    for(int i=s; i<=N-1; i++) if(v[i][1] != EMPTY) cnt++;
 
-    int tate_len = 0;
-    for(int i=0; v[start_idx][i] != EMPTY; i++) tate_len++;
+    int l2 = 0;
+    for(int i=0; v[s][i] != EMPTY; i++) l2++;
 
-    return (base_length - cnt) >= tate_len;
+    return (l1 - cnt) >= l2;
 }
 
 void first_lift_up(){
-    if(!check_first_list_up_pos()) return;
-    v[start_idx+1][1] = v[start_idx][0];
-    v[start_idx][0] = EMPTY; start_idx++;
+    if(!check_first_lift_up_pos()) return;
+    v[s+1][1] = v[s][0];
+    v[s][0] = EMPTY; s++;
 
-    while(check_first_list_up_pos()){
-        int next_start_idx = start_idx;
-        vector<vector<int>> sv;
+    while(check_first_lift_up_pos()){
+        int next_s = s;
+        vvi sv;
 
-        for(int i=start_idx; i<=end_idx; i++){
+        for(int i=s; i<=N-1; i++){
             if(v[i][1] == EMPTY) continue;
-            next_start_idx = i;
+            next_s = i;
 
-            vector<int> tmp;
+            vi tmp;
             for(int j=0; v[i][j] != EMPTY; j++){
                 tmp.push_back(v[i][j]);
                 v[i][j] = EMPTY;
@@ -64,38 +60,38 @@ void first_lift_up(){
             sv.push_back(tmp);
         }
 
-        start_idx = next_start_idx + 1;
+        s = next_s + 1;
         for(int i=0; i<sv.size(); i++){
             int target_line = sv.size()-i;
-            for(int j=start_idx; j<start_idx+sv[i].size(); j++){
-                v[j][target_line] = sv[i][j-start_idx];
+            for(int j=s; j<s+sv[i].size(); j++){
+                v[j][target_line] = sv[i][j-s];
             }
         }
     }
 }
 
-void flattening(){
-    vector<int> tmp;
+void flatten(){
+    vi tmp;
 
-    for(int i=start_idx; i<=end_idx; i++) {
+    for(int i=s; i<=N-1; i++) {
         for(int j=0; v[i][j] != EMPTY; j++) {
             tmp.push_back(v[i][j]);
             v[i][j] = EMPTY;
         }
     }
-    start_idx = 0;
+    s = 0;
     for(int i=0; i<N; i++){
         v[i][0] = tmp[i];
     }
 }
 
-void adjacent_aligning(){
+void align(){
     int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, 1, -1};
 
-    vector<vector<int>> dv;
-    for(int i=start_idx; i<=end_idx; i++){
-        vector<int> tmp;
+    vvi dv;
+    for(int i=s; i<=N-1; i++){
+        vi tmp;
 
         for(int j=0; v[i][j] != EMPTY; j++){
             int diff = 0;
@@ -104,7 +100,7 @@ void adjacent_aligning(){
                 int target_j = j + dy[k];
 
                 if(target_i < 0 || target_j < 0 || v[target_i][target_j] == EMPTY) continue;
-                diff += (v[target_i][target_j] - v[i][j]) / DIV;
+                diff += (v[target_i][target_j] - v[i][j]) / 5;
             }
             tmp.push_back(diff);
         }
@@ -113,30 +109,30 @@ void adjacent_aligning(){
 
     for(int i=0; i<dv.size(); i++){
         for(int j=0; j<dv[i].size(); j++){
-            v[i+start_idx][j] += dv[i][j];
+            v[i+s][j] += dv[i][j];
         }
     }
 }
 
 void second_lift_up(){
-    int next_start_idx = start_idx + N/2;
+    int next_s = s + N/2;
     for(int i=N/2-1; i>=0; i--){
-        v[next_start_idx+(N/2-1-i)][1] = v[i][0];
+        v[next_s+(N/2-1-i)][1] = v[i][0];
         v[i][0] = EMPTY;
     }
-    start_idx = next_start_idx;
+    s = next_s;
 
     vector<pair<int, int>> tmp;
-    next_start_idx = start_idx + N/4;
-    for(int i=next_start_idx-1; i>=start_idx; i--){
+    next_s = s + N/4;
+    for(int i=next_s-1; i>=s; i--){
         tmp.push_back({v[i][1], v[i][0]});
         v[i][0] = v[i][1] = EMPTY;
     }
-    start_idx = next_start_idx;
+    s = next_s;
 
-    for(int i=start_idx; i<=end_idx; i++){
-        v[i][2] = tmp[i-start_idx].first;
-        v[i][3] = tmp[i-start_idx].second;
+    for(int i=s; i<=N-1; i++){
+        v[i][2] = tmp[i-s].first;
+        v[i][3] = tmp[i-s].second;
     }
 }
 
@@ -145,7 +141,6 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> N >> K;
-    start_idx = 0, end_idx = N-1;
 
     for(int i=0; i<N; i++) {
         int a; cin >> a;
@@ -156,11 +151,11 @@ int main(){
     while(get_diff() > K){
         minus_all_min_idx();
         first_lift_up();
-        adjacent_aligning();
-        flattening();
+        align();
+        flatten();
         second_lift_up();
-        adjacent_aligning();
-        flattening();
+        align();
+        flatten();
         cnt++;
     }
     cout << cnt;
