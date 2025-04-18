f19776ac158c77691ff99c9e5285aa583acc03ea
박준일_T7154 <116266030+june21a@users.noreply.github.com>
2025-04-18 12:51:38 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.cc
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#define ll long long
 
 using namespace std;
 
@@ -12,8 +11,7 @@ int main(){
     int N, M; cin >> N >> M;
     int DP[N+1];
     DP[0] = DP[1] = 1;
-    DP[2] = 2;
-    for(int i=3; i<=N; i++) DP[i] = DP[i-1]+DP[i-2];
+    for(int i=2; i<=N; i++) DP[i] = DP[i-1]+DP[i-2];
     
     int s = 1, e = N;
     int ans = 1;
