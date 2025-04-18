c4358932a547adcabc68934cc8b82d5c324bb97e
박준일_T7154 <116266030+june21a@users.noreply.github.com>
2025-04-18 12:48:16 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#define ll long long
+
+using namespace std;
+
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M; cin >> N >> M;
+    int DP[N+1];
+    DP[0] = DP[1] = 1;
+    DP[2] = 2;
+    for(int i=3; i<=N; i++) DP[i] = DP[i-1]+DP[i-2];
+
+
+    if(M == 0){
+        cout << DP[N];
+    }
+    else{
+        int s = 1, e = N;
+        int ans = 1;
+        for(int i=0; i<M; i++){
+            int num; cin>>num;
+            ans *= DP[num-s];
+            s = num+1;
+        }
+        ans *= DP[max(0, e-s+1)];
+        cout << ans;
+    }
+}
